d92f742794047194427eeb9a190a507b53b5c285
aleksl0l <aleksl0l@yandex.ru>
2018-09-11 22:56:19 +0300

    change project structure

--- app/config.go
+++ app/config.go
@@ -1,9 +1,11 @@
 package app
 
-const (
-	DB_HOST     = "localhost"
-	DB_PORT     = "5432"
-	DB_NAME     = "books"
-	DB_USER     = "books"
-	DB_PASSWORD = "books"
+import "os"
+
+var (
+	DB_HOST     = os.Getenv("DB_HOST")
+	DB_PORT     = os.Getenv("DB_PORT")
+	DB_NAME     = os.Getenv("DB_NAME")
+	DB_USER     = os.Getenv("DB_USER")
+	DB_PASSWORD = os.Getenv("DB_PASSWORD")
 )
