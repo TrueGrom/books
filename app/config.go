78d561d79fbc95ecf5ee84ed39bac9da5f684a3c
True Grom <truegrom@gmail.com>
2018-09-11 22:40:17 +0300

    disable ssl mode

--- app/config.go
+++ app/config.go
@@ -4,6 +4,7 @@ const (
 	DB_HOST     = "localhost"
 	DB_PORT     = "5432"
 	DB_NAME     = "books"
-	DB_USER     = "books"
-	DB_PASSWORD = "books"
+	DB_USER     = "book"
+	DB_PASSWORD = "book"
+	DB_SSL_MODE = "disable"
 )
